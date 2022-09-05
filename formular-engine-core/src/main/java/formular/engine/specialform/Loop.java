2e1dbaf7bf38bceca1b3e460ac90b42f1f290aff
huanghao <huanghao@youxin.cloud>
2022-09-05 11:17:51 +0800

    empty loop returns null

--- formular-engine-core/src/main/java/formular/engine/specialform/Loop.java
+++ formular-engine-core/src/main/java/formular/engine/specialform/Loop.java
@@ -7,11 +7,16 @@ import formular.engine.Engine;
 import formular.engine.Environment;
 import formular.engine.Expression;
 import formular.engine.SpecialForm;
+import formular.engine.Util;
 
 public class Loop extends SpecialForm {
 
     @Override
     public Expression evaluate(List<Expression> args, Environment env, Debugger debugger, int depth) throws Exception {
+
+        if (args.isEmpty()) {
+            return Util.expressionOf(null);
+        }
 
         // Each form is evaluated in turn from left to right. When the last form has been evaluated,
         // then the first form is evaluated again, and so on, in a never-ending cycle.
